ca21df5848c8249aa558f4e868eb1b45cc9e9064
Laura <laurasimila95@gmail.com>
2026-01-09 14:29:26 +0200

    Kommentoitu middleware/AppError.js

--- backend/middleware/AppError.js
+++ backend/middleware/AppError.js
@@ -1,9 +1,17 @@
+// Tämä tiedosto määrittelee oman virheluokan, jota käytetään sovelluksen virheiden käsittelyssä
+
+// Luo uuden virheluokan, perii JavaScriptin sisäänrakennetun Error-luokan ominaisuudet
 class AppError extends Error {
-  constructor(message, statusCode) {
-    super(message);
-    this.statusCode = statusCode;
-    this.isOperational = true;
-  }
+    // Konstruktorissa määritellään virheviesti ja HTTP-statuskoodi
+    constructor(message, statusCode) {
+        // Kutsuu vanhemman luokan (Error) konstruktorin. Asettaa virheviestin. Mahdollistaa automaattisen stack-tracen luomisen
+        super(message);
+        // Tallentaa HTTP-statuskoodin virheeseen ja Error handler lukee sen sieltä
+        this.statusCode = statusCode;
+        // Merkitsee virheen hallittavaksi / odotetuksi. Esimerkkejä -> virheellinen syöte, ei oikeuksia ja ei löydy
+        this.isOperational = true;
+    }
 }
 
+// Vie luokan käyttöön muualla, esim. controllerissa
 module.exports = AppError;
