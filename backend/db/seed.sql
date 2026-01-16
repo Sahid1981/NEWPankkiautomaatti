1a3d8bbddc789d1145d468aec084a91d8179f4fc
Laura <laurasimila95@gmail.com>
2026-01-16 16:17:58 +0200

    Pientä fiksailua

--- backend/db/seed.sql
+++ backend/db/seed.sql
@@ -30,22 +30,22 @@ SET @credit_account_id = LAST_INSERT_ID();
 -- Kortti, is_locked = 0
 INSERT INTO cards (idcard, cardPIN, iduser, is_locked)
 VALUES (
-  'CARD123456',
-  '$2b$10$raFg/F.nby0A9jB1VLCgJOZrrwjXq8igIxcrAyHed20Ev4pnmLwOS',
-  'TESTUSER1',
-  0
+    'CARD123456',
+    '$2b$10$raFg/F.nby0A9jB1VLCgJOZrrwjXq8igIxcrAyHed20Ev4pnmLwOS',
+    'TESTUSER1',
+    0
 );
 
 -- Kaksoiskortti
 INSERT INTO accounts_cards (idaccount, idcard)
 VALUES
-  (@debit_account_id, 'CARD123456'),
-  (@credit_account_id, 'CARD123456');
+(@debit_account_id, 'CARD123456'),
+(@credit_account_id, 'CARD123456');
 
 -- Tapahtumia log-tauluun
 INSERT INTO log (idaccount, time, balancechange)
 VALUES
-  (@debit_account_id,  NOW(6) - INTERVAL 3 DAY,  500.00),
-  (@debit_account_id,  NOW(6) - INTERVAL 2 DAY,  -20.00),
-  (@debit_account_id,  NOW(6) - INTERVAL 1 DAY,  -40.00),
-  (@credit_account_id, NOW(6) - INTERVAL 1 DAY, -100.00);
+(@debit_account_id,  NOW(6) - INTERVAL 3 DAY,  500.00),
+(@debit_account_id,  NOW(6) - INTERVAL 2 DAY,  -20.00),
+(@debit_account_id,  NOW(6) - INTERVAL 1 DAY,  -40.00),
+(@credit_account_id, NOW(6) - INTERVAL 1 DAY, -100.00);
